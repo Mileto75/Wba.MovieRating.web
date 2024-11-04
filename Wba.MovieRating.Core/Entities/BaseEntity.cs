f1ad7ea030321e2253be64ba935e0e97d8e6bb0e
Mileto Di Marco <mileto.di.marco@howest.be>
2024-11-04 15:23:55 +0100

    Add entities

---
+++ Wba.MovieRating.Core/Entities/BaseEntity.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wba.MovieRating.Core.Entities
+{
+    public abstract class BaseEntity
+    {
+        public int Id { get; set; }
+        public DateTime Created { get; set; }
+        public DateTime? Updated { get; set; }
+        public DateTime? Deleted { get; set; }
+       
+    }
+}
