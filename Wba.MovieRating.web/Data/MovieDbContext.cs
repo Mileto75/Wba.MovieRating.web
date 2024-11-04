abd66ab48ac75941379147f27c99ad471a9fe2ea
Mileto Di Marco <mileto.di.marco@howest.be>
2024-11-04 15:27:34 +0100

    Add dbcontext

---
+++ Wba.MovieRating.web/Data/MovieDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Wba.MovieRating.Core.Entities;
+
+namespace Wba.MovieRating.Web.Data
+{
+    public class MovieDbContext : DbContext
+    {
+        //define the tables
+        public DbSet<Movie> Movies { get; set; }
+        public DbSet<User> Users { get; set; }
+        public MovieDbContext(DbContextOptions<MovieDbContext> options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //configure the database tables
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
