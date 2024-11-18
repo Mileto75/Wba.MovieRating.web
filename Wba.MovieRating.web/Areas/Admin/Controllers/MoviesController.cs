00383006fb2eb7a384ed16c8fe115bcb8ccd24f7
Mileto Di Marco <mileto.di.marco@howest.be>
2024-11-18 16:31:06 +0100

    Add info method

--- Wba.MovieRating.web/Areas/Admin/Controllers/MoviesController.cs
+++ Wba.MovieRating.web/Areas/Admin/Controllers/MoviesController.cs
@@ -3,6 +3,7 @@ using Microsoft.EntityFrameworkCore;
 using System.Security.Cryptography.Xml;
 using Wba.MovieRating.Web.Areas.Admin.ViewModels;
 using Wba.MovieRating.Web.Data;
+using static System.Net.WebRequestMethods;
 
 namespace Wba.MovieRating.Web.Areas.Admin.Controllers
 {
@@ -36,8 +37,44 @@ namespace Wba.MovieRating.Web.Areas.Admin.Controllers
         public async Task<IActionResult> Info(int id)
         {
             //get the movie
-            //
-            return View();
+            var movie = await _movieDbContext
+                .Movies
+                .Include(m => m.Company)
+                .Include(m => m.Actors)
+                .ThenInclude(a => a.Actor)
+                .Include(m => m.Directors)
+                .AsSplitQuery()//to use separate queries
+                .FirstOrDefaultAsync(m => m.Id == id);
+            //check if null
+            if (movie == null)
+            {
+                return NotFound();
+            }
+            //fill the model
+            var movieInfoViewModel = new MoviesInfoViewModel
+            {
+                Id = movie.Id,
+                Value = movie.Title,
+                Company = new BaseViewModel
+                {
+                    Id = movie.Company.Id,
+                    Value = movie.Company.Name,
+                },
+                Actors = movie.Actors.Select(a => new BaseViewModel
+                {
+                    Id = a.Actor.Id,
+                    Value = $"{a.Actor.Firstname} {a.Actor.Lastname}"
+                }),
+                Directors = movie.Directors.Select(d => new BaseViewModel
+                {
+                    Id = d.Id,
+                    Value = $"{d.Firstname} {d.Lastname}"
+                }),
+                Image = "https://placehold.co/600x400"
+
+            };
+            //pass to the view
+            return View(movieInfoViewModel);
         }
     }
 }
